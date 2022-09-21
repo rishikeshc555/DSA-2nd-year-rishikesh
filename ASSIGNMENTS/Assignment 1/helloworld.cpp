bc1ad527c5f69d50c70e5ee670e8d9d872e03d5c
rishikeshc555 <114002028+rishikeshc555@users.noreply.github.com>
2022-09-21 23:24:18 +0530

    Create helloworld.cpp

---
+++ ASSIGNMENTS/Assignment 1/helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+
+
+// https://www.hackerrank.com/challenges/cpp-hello-world/submissions/code/291817008
