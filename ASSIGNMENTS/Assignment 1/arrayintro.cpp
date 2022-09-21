bc9be36060d64d6942ea8c2c70eea1590333a4ec
rishikeshc555 <114002028+rishikeshc555@users.noreply.github.com>
2022-09-21 23:42:37 +0530

    Create arrayintro.cpp

---
+++ ASSIGNMENTS/Assignment 1/arrayintro.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int size = 0;
+    int value = 0;
+    cin >> size;
+    int arr[size];
+    for(int i = 0; i< size; i++){
+        cin >> arr[i];
+    }
+    for(int i = size - 1; i>=0; i--){
+        cout <<arr[i] <<" ";
+    }
+    
+    return 0;
+}
+
+// https://www.hackerrank.com/challenges/arrays-introduction/submissions/code/291830238
