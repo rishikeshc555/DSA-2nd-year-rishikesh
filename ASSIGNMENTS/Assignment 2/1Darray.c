e69677bdb1322241b8e88418f76e99fc16c6ea28
rishikeshc555 <114002028+rishikeshc555@users.noreply.github.com>
2022-09-25 23:22:00 +0530

    Create 1Darray.c

---
+++ ASSIGNMENTS/Assignment 2/1Darray.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for (int i=0; i<n; ++i) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int sum = 0;
+    for (int i=0; i<n; ++i) {
+        sum += arr[i];
+    }
+    printf("%d", sum);   
+    return 0;
+}
+
+
+// https://www.hackerrank.com/challenges/1d-arrays-in-c/submissions/code/292527031
