fdf1371a7aa98b289157c6a2efccbd48266f007e
rishikeshc555 <114002028+rishikeshc555@users.noreply.github.com>
2022-09-25 23:10:48 +0530

    Create helloworld.c

---
+++ ASSIGNMENTS/Assignment 2/helloworld.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("Hello, World! \n");
+    printf("Welcome to C programming.");
+    return 0;
+}
+
+
+// https://www.hackerrank.com/challenges/hello-world-c/submissions/code/292448223
