36739c71ba88e53c085a0b77c6c1659e55ad66ec
rishikeshc555 <114002028+rishikeshc555@users.noreply.github.com>
2023-01-17 02:32:18 +0530

    Create sum_of_unique_elements.cpp

---
+++ ASSIGNMENTS/Assignment 9/sum_of_unique_elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+         unordered_map<int, int> m;
+        for(int i=0; i<nums.size(); i++){
+            m[nums[i]]++;
+        }
+        
+        int sum=0;
+        for(int i=0; i<nums.size(); i++){
+           if(m[nums[i]]<=1){
+             sum += nums[i];
+           }
+        }
+      return sum;
+    }
+};
+
+
+
+// https://leetcode.com/problems/sum-of-unique-elements/submissions/879475937/
