74de7e036e349e4a6f07bf5c3cdcf54335ecc46d
rishikeshc555 <114002028+rishikeshc555@users.noreply.github.com>
2023-01-17 02:26:51 +0530

    Create number_of_good_pairs.cpp

---
+++ ASSIGNMENTS/Assignment 9/number_of_good_pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+         unordered_map<int, int> map;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            map[nums[i]]++;
+            count += map[nums[i]] - 1;
+        }
+        return count;
+        
+    }
+};
+
+
+// https://leetcode.com/problems/number-of-good-pairs/submissions/879473429/
