3909cd450c53df54c0fb07ecdfc9f7374296e668
rishikeshc555 <114002028+rishikeshc555@users.noreply.github.com>
2023-01-17 02:29:43 +0530

    Create jewels_and_stones.cpp

---
+++ ASSIGNMENTS/Assignment 9/jewels_and_stones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+         unordered_map<char, int> jw;
+        int count = 0;
+
+        for(auto &it : jewels)
+        jw[it]++;
+
+        for(auto &it : stones)
+            if(jw[it]>0)
+                count++;
+        return count;
+        
+    }
+};
+
+
+// https://leetcode.com/problems/jewels-and-stones/submissions/879474861/
