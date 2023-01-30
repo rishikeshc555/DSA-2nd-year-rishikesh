7ce9e78bfb92cd19ead9cf0d4c0ae25716d913e2
rishikeshc555 <114002028+rishikeshc555@users.noreply.github.com>
2023-01-30 22:53:53 +0530

    Create  Find Players With Zero or One Losses.cpp

---
+++ ASSIGNMENTS/Assignment 9/Find Players With Zero or One Losses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        vector<vector<int>> res(2,vector<int>());
+        map<int,int> um;
+        set<int> us;
+        for(int i=0;i<matches.size();i++)
+        {
+            int win=matches[i][0],los=matches[i][1];
+            um[los]++;
+            us.insert(win);
+            us.insert(los);
+        }
+        for(auto i:um) if(i.second==1) res[1].push_back(i.first);
+        for(auto i=us.begin();i!=us.end();i++)
+            if(um.find(*i)==um.end())
+                res[0].push_back(*i);
+        return res;
+        
+    }
+};
+
+
+//https://leetcode.com/problems/find-players-with-zero-or-one-losses/submissions/
